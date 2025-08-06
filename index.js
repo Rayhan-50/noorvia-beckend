883d62d6f7bab4076c08832b0519825efde88959
Rayhan-50 <rayhanahmed.nstu@gmail.com>
2025-08-06 19:11:18 +0600

    connect frontend

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
 
     const userCollection = client.db("noorvia").collection("users");
    
